c00e2c9a9f2e392a10013f3d737c75dc7c36e874
Majid <mkhan3189@gmail.com>
2018-03-29 22:53:59 +1100

    test

---
+++ test/index.js
@@ -0,0 +1,60 @@
+const easyloggingpp = require('../');
+const ConfigType = easyloggingpp.ConfigType;
+const Level = easyloggingpp.Level;
+
+// alternatively you will do
+// import 
+
+const logger = easyloggingpp.getLogger('mylogger');
+
+logger.configure({
+    config_file: 'test/easylogging-config.conf'
+});
+
+logger.info('info log after first config');
+
+// or you can use config types
+logger.configure({
+    config: ConfigType.Format,
+    value: '%datetime %msg',
+});
+
+logger.info('info log');
+
+// register new logger
+
+const logger2 = easyloggingpp.getLogger('another_logger');
+
+logger2.info('info log');
+
+easyloggingpp.configureAllLoggers({
+    config: ConfigType.Format,
+    value: '%levshort %datetime %fbase:%line %msg',
+});
+
+easyloggingpp.configureAllLoggers({
+    level: Level.Debug,
+    config: ConfigType.Format,
+    value: '%datetime %fbase:%line %msg',
+});
+
+// or you can use array
+
+easyloggingpp.configureAllLoggers([
+    {
+        level: Level.Error,
+        config: ConfigType.Format,
+        value: '%datetime >> %msg',
+    },
+    {
+        level: Level.Debug,
+        config: ConfigType.Format,
+        value: '%datetime %fbase:%line %msg',
+    }
+]);
+
+logger.info('after reconfigure');
+logger2.info('after reconfigure');
+logger2.debug('after reconfigure');
+logger2.error('after reconfigure');
+
